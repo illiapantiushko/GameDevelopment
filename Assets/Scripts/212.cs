313fca8cd19bb273f96518e5493b9fb5f5750e11
Pantiushko Illia <illia.pantiushko@oa.edu.ua>
2020-09-07 18:38:46 +0300

    initial commit

---
+++ Assets/Scripts/212.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 212 : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rigidbody=GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX
+         if(Input.GetKeyDown(KeyCode.Space)){
+             Debug.Log("Space")
+         }
+    }
+}
